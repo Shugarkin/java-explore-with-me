a1ba573140b8d8ae452e603196f5b265773b6aa7
Shugarkin <alexandr.shugarkin@yandex.ru>
2023-08-02 21:48:33 +0300

    feat: вроде все получилось

--- state/stat_service/src/main/java/ru/practicum/Main.java
+++ state/stat_service/src/main/java/ru/practicum/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StatDto {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(StatDto.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
