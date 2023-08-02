20cfce6c4d2cca55b38e46dce897755176d886ba
Shugarkin <alexandr.shugarkin@yandex.ru>
2023-08-02 17:46:36 +0300

    пока что ве что смог

--- state/stat_service/src/main/java/ru/practicum/StatService.java
+++ state/stat_service/src/main/java/ru/practicum/StatService.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class StatService {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication.run(StatService.class, args);
     }
 }
