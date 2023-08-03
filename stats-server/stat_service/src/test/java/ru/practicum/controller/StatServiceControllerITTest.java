170ca24b0682e5c93ebe83c47ac3e29907b65dd0
shugarkin <alexandr.shugarkin@yandex.ru>
2023-08-03 15:58:11 +0300

    style: удалил импорт

--- stats-server/stat_service/src/test/java/ru/practicum/controller/StatServiceControllerITTest.java
+++ stats-server/stat_service/src/test/java/ru/practicum/controller/StatServiceControllerITTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.RequestParam;
 import ru.practicum.dto.StatDto;
 import ru.practicum.mapper.StatMapper;
 import ru.practicum.model.Stat;
